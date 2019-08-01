6b224123dda2a3f3784b959235743208fdb9c150
guangtian.li <guangtian_li@qq.com>
2019-08-01 16:59:42 +0800

    :white_check_mark: improve tests

--- tests/contrib/test_apollo.py
+++ tests/contrib/test_apollo.py
@@ -6,19 +6,40 @@ import requests
 
 from configalchemy.contrib.apollo import ApolloBaseConfig
 
+return_value = {"namespaceName": "tmp", "configurations": {"TEST": "changed"}}
+
+
+def mock_get(url: str, params: dict = None, timeout: int = 5):
+    if "notifications" in url:
+        return Mock(
+            status_code=200,
+            ok=False,
+            json=Mock(
+                return_value=[{"namespaceName": "application", "notificationId": 1}]
+            ),
+        )
+    else:
+        return Mock(status_code=200, ok=True, json=Mock(return_value=return_value))
+
 
 class ApolloConfigTestCase(unittest.TestCase):
     def test_ApolloConfig(self):
+        requests.get = mock_get
+        long_poll = Mock()
+        ApolloBaseConfig.start_long_poll = long_poll
+
         class DefaultConfig(ApolloBaseConfig):
             TEST = "base"
+            ENABLE_LONG_POLL = True
             #: apollo
-            APOLLO_SERVER_URL = "http://apollo.service.gllue.net"
-            APOLLO_APP_ID = "5b366db80139"
+            APOLLO_SERVER_URL = ""
+            APOLLO_APP_ID = ""
             APOLLO_CLUSTER = "default"
             APOLLO_NAMESPACE = "application"
 
         config = DefaultConfig()
-        self.assertEqual("test", config["TEST"])
+        self.assertEqual("changed", config["TEST"])
+        self.assertEqual(1, long_poll.call_count)
 
     def test_long_poll(self):
         class DefaultConfig(ApolloBaseConfig):
@@ -30,24 +51,6 @@ class ApolloConfigTestCase(unittest.TestCase):
             APOLLO_NAMESPACE = "application"
 
         config = DefaultConfig()
-        return_value = {"namespaceName": "tmp", "configurations": {"TEST": "changed"}}
-
-        def mock_get(url: str, params: dict = None, timeout: int = 5):
-            if "notifications" in url:
-                return Mock(
-                    status_code=200,
-                    ok=False,
-                    json=Mock(
-                        return_value=[
-                            {"namespaceName": "application", "notificationId": 1}
-                        ]
-                    ),
-                )
-            else:
-                return Mock(
-                    status_code=200, ok=True, json=Mock(return_value=return_value)
-                )
-
         requests.get = mock_get
         self.assertEqual("test", config["TEST"])
         self.assertEqual("changed", config.get_from_namespace("TEST", namespace="tmp"))
