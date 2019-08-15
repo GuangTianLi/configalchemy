0e51bff04f50f4600f1b562f419c6a380131e968
guangtian.li <guangtian_li@qq.com>
2019-08-15 18:03:43 +0800

    :bug: fix test

--- tests/test_global.py
+++ tests/test_global.py
@@ -1,10 +1,13 @@
 import unittest
 
 from configalchemy import get_current_config, BaseConfig
+from configalchemy import configalchemy
 
 
 class GlobalAPITestCase(unittest.TestCase):
     def test_get_current_config(self):
+        configalchemy._current_config = None
+
         class DefaultConfig(BaseConfig):
             TEST = "test"
 
